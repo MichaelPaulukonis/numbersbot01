183def78c2a3ab9929bdedce8246530accefcb42
Michael Paulukonis <mpauluko@travelers.com>
2019-06-06 20:20:11 -0400

    Spcified 8.19 engine, cleanup, runs local w/o config

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var config = require('./config.js');
 var Twit = require('twit');
-var T = new Twit(config);
 
 // 1..999999 so 1..6 digit numbers are possible this way
 // previous version ALWAYS returned six-digits
@@ -8,7 +7,9 @@ const getNumbers = () => parseInt(Math.random() * 1000000, 10)
 
 const tweeter = () => {
   const newSentence = getNumbers();
+  console.log(`The number: ${newSentence}`)
   if (config.tweet_on) {
+    const T = new Twit(config);
     T.post('statuses/update', { status: newSentence }, (err, reply) => {
       if (err) console.log('error:', err);
     });
